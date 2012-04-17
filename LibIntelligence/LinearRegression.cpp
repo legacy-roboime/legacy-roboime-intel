07e1304c7dac48bd36072d54133a11f6a4896f7a
Diego Felix <diegobill@gmail.com>
2012-04-17 01:26:50 +0000

    Arquivos de config do projeto (inclusao de arquivos no projeto).
    git-svn-id: http://roboime.com.br/svn/RoboCup/Projeto Computacional/RobotIME/branches/bgt@690 536dbed5-76cd-454d-90a9-4f62b6fdd354

---
+++ LibIntelligence/LinearRegression.cpp
@@ -0,0 +1,65 @@
+#include "LinearRegression.h"
+
+LinearRegression::LinearRegression(QPointF *p, long size, unsigned char max_size_points)
+	: xPoints(new QVector<double>()), yPoints(new QVector<double>())
+{
+	MAX_SIZE_POINTS = max_size_points;
+
+	for(int i = 0; i < MAX_SIZE_POINTS; i++){
+		xPoints->push_back(0.);
+		yPoints->push_back(0.);
+	}
+	long i;
+	a = b = sumX = sumY = sumXsquared = sumYsquared = sumXY = 0.0;
+	n = 0L;
+
+	if (size > 0L) // if size greater than zero there are data arrays
+		for (n = 0, i = 0L; i < size; i++)
+			addPoint(p[i]);
+}
+
+
+//modificado
+void LinearRegression::addXY(const double& x, const double& y)
+{
+	n++;
+	double xOld = xPoints->at(n % MAX_SIZE_POINTS);
+	double yOld = yPoints->at(n % MAX_SIZE_POINTS);
+	xPoints->replace(n % MAX_SIZE_POINTS,x);
+	yPoints->replace(n % MAX_SIZE_POINTS,y);
+	sumX += x - xOld;
+	sumY += y - yOld;
+	sumXsquared += x * x - xOld*xOld;
+	sumYsquared += y * y - yOld*yOld;
+	sumXY += x * y - xOld*yOld;
+	Calculate();
+
+	//for(int i = 0; i < MAX_SIZE_POINTS; i++ )	cout << xPoints->at(i) << ";"  << yPoints->at(i) << endl;
+
+	//cout << "-----------------------------------------------------" << endl;
+}
+
+void LinearRegression::Calculate()
+{
+	if (haveData())
+	{
+		if (fabs( double(n) * sumXsquared - sumX * sumX) > DBL_EPSILON)
+		{
+			b = ( double(n) * sumXY - sumY * sumX) /
+				( double(n) * sumXsquared - sumX * sumX);
+			a = (sumY - b * sumX) / double(n);
+
+			double sx = b * ( sumXY - sumX * sumY / double(n) );
+			double sy2 = sumYsquared - sumY * sumY / double(n);
+			double sy = sy2 - sx;
+
+			coefD = sx / sy2;
+			coefC = sqrt(coefD);
+			stdError = sqrt(sy / double(n - 2));
+		}
+		else
+		{
+			a = b = coefD = coefC = stdError = 0.0;
+		}
+	}
+}
---
+++ LibIntelligence/LinearRegression.h
@@ -0,0 +1,59 @@
+#pragma once
+
+//http://howtobyexamples.blogspot.com.br/2011/10/c-linear-regression-source-code.html
+
+#ifndef LINEARREGRESSION_H
+#define LINEARREGRESSION_H
+
+#include <qpoint.h>
+#include "qvector.h"
+#include <iostream>
+
+using namespace std;
+
+class LinearRegression
+{
+public:
+	// Constructor using an array of QPoint objects
+	// This is also the default constructor
+	LinearRegression(QPointF *p = 0, long size = 0, unsigned char max_size_points = 20);
+
+	virtual void addXY(const double& x, const double& y);
+	//void clear() { sumX = sumY = sumXsquared = sumYsquared = sumXY = n = 0; }
+	void addPoint(const QPointF& p) { addXY(p.x(), p.y()); }
+
+	// Must have at least 3 points to calculate
+	// standard error of estimate. Do we have enough data?
+	int haveData() const { return (n > 2 ? 1 : 0); }
+	long items() const { return n; }
+
+	virtual double getA() const { return a; }
+	virtual double getB() const { return b; }
+
+	double getCoefDeterm() const { return coefD; }
+	double getCoefCorrel() const { return coefC; }
+	double getStdErrorEst() const { return stdError; }
+	virtual double estimateY(double x) const { return (a + b * x); }
+	QPointF getLastPointInserted(){ return QPointF(xPoints->at(n % MAX_SIZE_POINTS), yPoints->at(n % MAX_SIZE_POINTS));}
+
+
+protected:
+	long n;       // number of data points input so far
+	double sumX, sumY; // sums of x and y
+	QVector<double>* xPoints;
+	QVector<double>* yPoints;
+	double sumXsquared, // sum of x squares
+		sumYsquared; // sum y squares
+	double sumXY;    // sum of x*y
+
+	double a, b;    // coefficients of f(x) = a + b*x
+	double coefD,    // coefficient of determination
+		coefC,    // coefficient of correlation
+		stdError;  // standard error of estimate
+	void Calculate();  // calculate coefficients
+
+private:
+	unsigned char MAX_SIZE_POINTS;
+};
+
+#endif LINEARREGRESSION_H
