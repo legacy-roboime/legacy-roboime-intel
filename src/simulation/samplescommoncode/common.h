11636817ba1ad34ca935153daca9a5cb1fbb1675
Jan Segre <jan@segre.in>
2012-05-01 14:11:59 -0300

    Projeto inteiro compilando com o cmake. Woohoogit status | egrep modified

---
+++ src/simulation/samplescommoncode/common.h
@@ -0,0 +1,28 @@
+#ifndef COMMON_HEADER_FILE
+#define COMMON_HEADER_FILE
+
+#include <GL/GL.h>
+
+#ifndef __PPCGEKKO__
+inline void glVertexPointer (GLint size, GLenum type, GLsizei stride, GLuint buffer_size, const GLvoid *pointer)
+{
+	glVertexPointer(size, type, stride, pointer);
+}
+
+inline void glTexCoordPointer (GLint size, GLenum type, GLsizei stride, GLuint buffer_size, const GLvoid *pointer)
+{
+	glTexCoordPointer(size, type, stride, pointer);
+}
+
+inline void glNormalPointer (GLenum type, GLsizei stride, GLuint buffer_size, const GLvoid *pointer)
+{
+	glNormalPointer(type, stride, pointer);
+}
+
+inline void glColorPointer (GLint size, GLenum type, GLsizei stride, GLuint buffer_size, const GLvoid *pointer)
+{
+	glColorPointer(size, type, stride, pointer);
+}
+#endif
+
+#endif
