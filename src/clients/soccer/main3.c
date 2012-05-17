632d9f5e845ecd6fd6698ab9c2da637ba04e253e
Felix <diegobill@gmail.com>
2012-05-17 16:13:23 -0300

    Soccer Client da Soccer 3.6 (com gravação)

--- src/clients/soccer/main3.c
+++ src/clients/soccer/main3.c
@@ -3,16 +3,40 @@
 
 static int winWidth, winHeight;
 static SoccerState *s, saux;
+static char *image;
 
+static void redraw( void );
+static void reshape(int wid, int ht);
+static void img_ppm_write( char *img, char *fname );
+
+#define WIDTH 900
+#define HEIGHT 300
+
+int main( int argc, char **argv )
+{
+ image = ( char* )malloc( 3*WIDTH*HEIGHT );
+ soccer_env_init();
+ s = sstate_alloc();
+ minimax_init(s);
+ glutInit(&argc, argv);
+ glutInitWindowSize(WIDTH ,HEIGHT);
+ glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE |  GLUT_DEPTH);
+ (void)glutCreateWindow("Soccer");
+ glutReshapeFunc(reshape);
+ glEnable(GL_DEPTH_TEST);
+ glutDisplayFunc(redraw);
+ glutIdleFunc(redraw);
+ glutMainLoop();
+ return 0;
+}
 
 void redraw( void ){
-   int i;
-   
+   static int count = 0;
+   char fname[50];
    SoccerAction red_action, blue_action;
+
    saux = *s;
    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-   minimax_use_next_red_robot();
-   minimax_use_next_blue_robot();
    minimax_play( &saux, MINIMAX_MAX_LEVEL );
    red_action = *minimax_get_best_red_action();
    blue_action = *minimax_get_best_blue_action();
@@ -20,12 +44,18 @@ void redraw( void ){
    saction_red_act( &saux, &red_action );
    glViewport(winWidth /2., 0, winWidth /2., winHeight ); 
    soccer_redraw( &saux ); 
-   //blue_action = saction_blue_make(s); 
-   saction_simulate( s, &red_action, &blue_action, .1 );
+  // usleep(1000000);
+   saction_simulate( s, &red_action, &blue_action, 
+                     soccer_env()->sample_period );
    glViewport(0, 0, winWidth/2., winHeight );   
    soccer_redraw( s );
+   glPixelStorei(GL_PACK_ALIGNMENT , 1);
+   glReadBuffer(GL_BACK);
    glutSwapBuffers();
-   //usleep(800000);
+   glReadPixels( 0, 0,  WIDTH, HEIGHT, GL_RGB,
+                 GL_UNSIGNED_BYTE, image );
+   sprintf( fname, "frame%i.ppm", count++ ); 
+   //img_ppm_write( image, fname );
 }
 
 
@@ -41,19 +71,17 @@ void reshape(int wid, int ht)
 }
 
 
-int main( int argc, char **argv )
+void img_ppm_write( char *img, char *fname )
 {
- soccer_env_init();
- s = sstate_alloc();
- minimax_init(s);
- glutInit(&argc, argv);
- glutInitWindowSize(1200, 400);
- glutInitDisplayMode(GLUT_RGB|GLUT_DEPTH|GLUT_DOUBLE);
- (void)glutCreateWindow("Soccer");
- glutReshapeFunc(reshape);
- glEnable(GL_DEPTH_TEST);
- glutDisplayFunc(redraw);
- glutIdleFunc(redraw);
- glutMainLoop();
- return 0;
+ int i;
+
+ FILE *fout = fopen( fname, "w" );
+ fprintf(fout, "P6\n");
+ fprintf(fout, "%d %d\n", WIDTH, HEIGHT );
+ fprintf(fout, "%d\n", 255);
+
+ for( i = 0; i < 3*WIDTH*HEIGHT; i++ )
+    fputc( img[i], fout ); 
+
+ fclose( fout );
 }
