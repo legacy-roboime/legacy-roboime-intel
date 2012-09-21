8c138510dab3592bee76ad32c87c6650059cd7f1
SuperPc <admin@roboime.com.br>
2012-09-21 17:14:35 -0300

    Erro, pois existe id de robo 0.

--- src/intelligence/core/plays/Minmax2.cpp
+++ src/intelligence/core/plays/Minmax2.cpp
@@ -180,11 +180,11 @@ void Minmax2::run()
 		*sL = *s;
 //#endif
 
-		if(sL->red_ball_owner>0){
+		if(sL->red_ball_owner>-1){
 			sL->ball_vel = v2_make(0,0);
 			sL->red[sL->red_ball_owner] = sL->ball;
 		}
-		else if(sL->blue_ball_owner>0){
+		else if(sL->blue_ball_owner>-1){
 			sL->ball_vel = v2_make(0,0);
 			sL->blue[sL->blue_ball_owner] = sL->ball;
 		}
