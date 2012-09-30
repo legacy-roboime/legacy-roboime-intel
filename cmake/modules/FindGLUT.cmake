f773f79acd048c2bda21afc3d27dd252fbe0b974
Jan Segre <jan@segre.in>
2012-09-30 19:29:58 -0300

    Compiling on linux (Ubuntu 12.04)

--- cmake/modules/FindGLUT.cmake
+++ cmake/modules/FindGLUT.cmake
@@ -1,5 +1,67 @@
+if(WIN32)
+    find_path(GLUT_INCLUDE_DIR
+        NAMES GL/glut.h 
+        PATHS  ${GLUT_ROOT_PATH}/include
+    )
+    find_library(GLUT_glut_LIBRARY
+        NAMES glut glut32 freeglut
+        PATHS
+        ${OPENGL_LIBRARY_DIR}
+        ${GLUT_ROOT_PATH}/Release
+    )
+elseif(APPLE)
+    # These values for Apple could probably do with improvement.
+    find_path(GLUT_INCLUDE_DIR
+        glut.h
+        /System/Library/Frameworks/GLUT.framework/Versions/A/Headers
+        ${OPENGL_LIBRARY_DIR}
+    )
+    set(GLUT_glut_LIBRARY "-framework GLUT" CACHE STRING "GLUT library for OSX") 
+    set(GLUT_cocoa_LIBRARY "-framework Cocoa" CACHE STRING "Cocoa framework for OSX")
+else()
+    find_path(GLUT_INCLUDE_DIR
+        GL/glut.h
+        /usr/include/GL
+        /usr/openwin/share/include
+        /usr/openwin/include
+        /opt/graphics/OpenGL/include
+        /opt/graphics/OpenGL/contrib/libglut
+    )
+  
+    find_library(GLUT_glut_LIBRARY
+        glut
+        /usr/openwin/lib
+    )
+    
+    find_library(GLUT_Xi_LIBRARY
+        Xi
+        /usr/openwin/lib
+    )
+    
+    find_library(GLUT_Xmu_LIBRARY
+        Xmu
+        /usr/openwin/lib
+    )
+endif()
 
-if(NOT GLUT_FOUND)
+set(GLUT_FOUND NO)
+if(GLUT_INCLUDE_DIR)
+    if(GLUT_glut_LIBRARY)
+        # Is -lXi and -lXmu required on all platforms that have it?
+        # If not, we need some way to figure out what platform we are on.
+        set(GLUT_LIBRARIES
+            ${GLUT_glut_LIBRARY}
+            ${GLUT_Xmu_LIBRARY}
+            ${GLUT_Xi_LIBRARY} 
+            ${GLUT_cocoa_LIBRARY}
+        )
+        set(GLUT_FOUND YES)
+    
+        #The following deprecated settings are for backwards compatibility with CMake1.4
+        set(GLUT_LIBRARY ${GLUT_LIBRARIES})
+        set(GLUT_INCLUDE_PATH ${GLUT_INCLUDE_DIR})
+    endif()
+else()
     find_package(PhysX)
     if(PHYSX_FOUND)
         find_path(GLUT_INCLUDE_DIR
@@ -21,6 +83,7 @@ if(NOT GLUT_FOUND)
             PATH_SUFFIXES ${GLUT_PATH_SUFFIX}
             PATHS ${PHYSX_DIR}
         )
+    elseif()
     endif()
 endif()
 
@@ -34,5 +97,7 @@ find_package_handle_standard_args(GLUT DEFAULT_MSG GLUT_INCLUDE_DIR)
 mark_as_advanced(
     GLUT_INCLUDE_DIR
     GLUT_glut_LIBRARY
+    GLUT_Xmu_LIBRARY
+    GLUT_Xi_LIBRARY
 )
 
