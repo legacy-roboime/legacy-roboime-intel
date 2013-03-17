a37a2dbd8cf689e4803496b946bd78ee36c36f01
Jan Segre <jan@segre.in>
2013-03-17 19:31:10 -0300

    Actuator tests.

---
+++ tests/actuator_unittest.cc
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include <Actuator.h>
+#include <limits.h>
+
+using namespace LibIntelligence;
+
+class ActuatorTest : public testing::Test {
+ protected:
+
+  virtual void SetUp() {
+    actuator = new Actuator();
+  }
+
+  virtual void TearDown() {
+    delete actuator;
+  }
+
+  Actuator *actuator;
+};
+
+TEST_F(ActuatorTest, DefaultConstructor) {
+  EXPECT_EQ(0.0, actuator->speed());
+  EXPECT_EQ(NULL, actuator->parent());
+}
+
+TEST_F(ActuatorTest, SetSpeed) {
+  double speed;
+
+  speed = 666;
+  actuator->setSpeed(speed);
+  EXPECT_EQ(speed, actuator->speed());
+}
